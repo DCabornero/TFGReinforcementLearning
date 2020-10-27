9939d257ec74bb58c0ae04c3bb2334b7863ad8b1
DCabornero <dcabornero@gmail.com>
2020-10-28 00:28:21 +0100

    Queda acabar NB

--- ModeloJuguete/Arm.py
+++ ModeloJuguete/Arm.py
@@ -2,6 +2,8 @@ from abc import abstractmethod
 import numpy as np
 import pandas as pd
 
+from sklearn.preprocessing import normalize
+
 # Cada uno de los Arms tendrá un sistema de recomendación que permita
 # dar un elemento a recomendar para un cierto usuario siguiendo un cierto algoritmo.
 # El trainSet debe ser dado en formato de matriz numPy.
@@ -133,14 +135,62 @@ class ArmNB(Arm):
     # El trainSet está compuesto por tres columnas: userId, itemId, rating
     def __init__(self,trainSet):
         self.trainSet = trainSet
-        self.num_ratings = len(np.unique(trainSet[:,2]))
-        self.num_items = len(np.unique(trainSet[:,1]))
-        self.num_users = len(np.unique(trainSet[:,0]))
+        self.ratings = np.unique(trainSet[:,2])
+        self.num_ratings = len(self.ratings)
+        self.items = np.unique(trainSet[:,1])
+        self.num_items = len(self.items)
+        self.users = np.unique(trainSet[:,0])
+        self.num_users = len(self.users)
 
-    # Tabla de frecuencias de dimensión num_items x num_ratings
-    def fit_priores(self):
-        priores = np.zeros((self.num_items,self.num_ratings))
-        self.priores = pd.DataFrame(priores, columns=np.unique(self.trainSet[:,2]), index=np.unique(self.trainSet[:,1]))
-        # Rellenamos la tabla de frecuencias con los datos
-        for row in self.trainSet:
-            self.priores.loc[row[1],row[2]] += 1
+    # Se calculan los priores (ratings) para un cierto item
+    def priores(self, item):
+        priors = np.zeros(self.num_ratings)
+        for i, rt in enumerate(self.ratings):
+            mask = self.trainSet[:,1] == item
+            priors[i] = np.count_nonzero(self.trainSet[mask,2] == rt)
+        return priors/sum(priors)
+
+    # Un usario puntúa un item (1) puntuado por el target y ha valorado (2) el item deseado.
+    # Esta función devuelve ejemplos con dos columnas: el primero es el item 1 y el segundo
+    # es la valoración 2.
+    # item es el item 1 y itemsTarget es la lista de valoraciones de todos los usuarios teniendo
+    # solo en cuenta los items valorados por el target
+    def tablaCondicional(self,item,itemsTarget):
+        # Nos quedamos con los usuarios que han valorado el item y sus ratings
+        mask = self.trainSet[:,1] == item
+        users = self.trainSet[mask]
+        users = self.trainSet[:,[0,2]]
+
+        usersList = list(users[:,0])
+
+        # Filtramos itemsTarget a los usuarios que han valorado el item
+        func = lambda x: x[0] in usersList
+        mask = np.apply_along_axis(func,1,itemsTarget)
+        finalItems = itemsTarget[mask]
+
+        # Cambiamos el rating del item por el rating del item (1) del usuario
+        # Esta lambda obtiene el rating del item (1) del usuario
+        lmb = lambda x: users[usersList.index(x[0]),1]
+        ratings = np.apply_along_axis(lmb,1,finalItems)
+        finalItems[:,2] = ratings
+        return finalItems[:,[1,2]]
+
+    # De la tabla de resultados anterior obtenemos una tabla de frecuencias normalizada
+    # La dimensión de la tabla es numItems x num_ratings, donde numItems es el número de
+    # items distintos que haya en tablaCondicional. La normalización consiste en que las columnas suman 1.
+    def tablaFrecs(self,tCond):
+        listItems = list(np.unique(tCond[:,0]))
+        listRatings = list(self.ratings)
+        # Normalización de Lagrange por defecto
+        frecs = np.zeros((len(listItems),self.num_ratings))
+
+        unique, counts = np.unique(tCond,return_counts=True,axis=1)
+        # LE DA LA VUELTA AL PUTO ARRAY
+        print(tCond)
+        print(unique)
+        counts += 1
+        for i, u in enumerate(unique):
+            print(u)
+            frecs[listItems.index(u[0]),listRatings.index(u[1])] = counts[i]
+
+        return normalize(frecs, axis=1, norm='l1')
